2f33ffaf4db3ab5c6d367ca0c89dd487078beb0c
narukami894 <s.yakushiji@icloud.com>
2017-05-12 18:05:06 +0900

    commit 5/12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+  before_action :logged_in_user, :correct_user, only: %i[edit update]
+
+  def index; end
+
   def new
     @user = User.new
   end
@@ -14,11 +19,38 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy; end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 
-  private
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in.'
+      redirect_to log_in_url
+    end
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
