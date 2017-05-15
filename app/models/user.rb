eba6cb18c7bf4bebdfda8674056b23a8168847d3
narukami894 <s.yakushiji@icloud.com>
2017-05-15 13:11:42 +0900

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
-  before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -19,12 +20,28 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # def authenticated?(remember_token)
+  #   return false if remember_digest.nil?
+  #   BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # end
+
+  # 上記のコードを一般化
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
-  class << self # ここからはクラスメソッド
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  # クラスメソッド
+  class << self
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
@@ -34,5 +51,17 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
+  end
+
+  # プライベートメソッド
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,8 +1,9 @@
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @admin     = users(:hoge)
-    @non_admin = users(:piyo)
+    @admin         = users(:hoge)
+    @non_admin     = users(:piyo)
+    @non_activated = users(:aho)
   end
 
   test 'index as admin including pagination and delete links' do
@@ -10,7 +11,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
@@ -26,5 +27,17 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test 'non_activated users do not display in users/index' do
+    log_in_as(@admin)
+    get users_path
+    assert_select 'a[href=?]', user_path(@non_activated), count: 0
+  end
+
+  test 'cannot access to users/show/non_activated user' do
+    log_in_as(@admin)
+    get user_path(@non_activated)
+    assert_redirected_to root_url
   end
 end
