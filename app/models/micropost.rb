f5c840cb86a64816cb8b619faa93b34fd9acd380
narukami894 <s.yakushiji@icloud.com>
2017-05-16 15:37:37 +0900

    Done

---
+++ app/models/micropost.rb
@@ -0,0 +1,17 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates  :content, presence: true, length: { maximum: 140 }
+  validates  :user_id, presence: true
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  # アップロードされた画像のサイズをバリデーションする
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, 'should be less than 5MB')
+    end
+  end
+end
